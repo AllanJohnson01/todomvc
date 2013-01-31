435eacc8a837e55f3b041b36a1cce0a166ecf6cc
Juha Paananen <juha.paananen@gmail.com>
2013-02-01 00:12:41 +0200

    oh, the spec was simpler than I thought

--- labs/architecture-examples/baconjs/js/app.js
+++ labs/architecture-examples/baconjs/js/app.js
@@ -31,8 +31,13 @@ $(function() {
     }
   }
 
-  function TodoListView(listElement, model, selectedTodos, filterChanges) {
-    var repaint = model.clearCompleted.merge(filterChanges).merge(model.toggleCompleted)
+  function TodoListView(listElement, model, hash) {
+    var selectedTodos = hash.decode({
+      "#/": model.allTodos,
+      "#/active": model.activeTodos,
+      "#/completed": model.completedTodos
+    })
+    var repaint = model.clearCompleted.merge(hash.changes()).merge(model.toggleAll)
     repaint.map(selectedTodos).onValue(function(todos) {
       listElement.children().remove()
       _.each(todos, addTodo)
@@ -66,29 +71,17 @@ $(function() {
     model.clearCompleted.plug(element.asEventStream("click"))
   }
 
-  function ToggleAllView(element, model, selectedTodos) {
-    model.toggleCompleted.plug(selectedTodos.sampledBy(Bacon.UI.checkBoxValue(element).changes(), function(todos, completed) {
-      return { selectedTodos: todos, completed: completed }
-    }))
+  function ToggleAllView(element, model) {
+    model.toggleAll.plug(Bacon.UI.checkBoxValue(element).changes())
   }
 
-  function FilterView(element, model) {
-    var hash = Bacon.UI.hash("#/")
-    var selectedTodos = hash.decode({
-      "#/": model.allTodos,
-      "#/active": model.activeTodos,
-      "#/completed": model.completedTodos
-    })
+  function FilterView(element, hash) {
     hash.onValue(function(hash) {
       element.find("a").each(function() {
         var link = $(this)
         link.toggleClass("selected", link.attr("href") == hash)
       })
     })
-    return {
-      selectedTodos: selectedTodos,
-      changes: hash.changes()
-    }
   }
 
   function TodoCountView(element, model) {
@@ -102,25 +95,21 @@ $(function() {
       return _.map(todos, function(todo) { return todo.id === updatedTodo.id ? updatedTodo : todo })
     }
     function toggleCompleted(todos, toggle) {
-      var ids = _.pluck(toggle.selectedTodos, "id")
       return _.map(todos, function(todo) { 
-        if (_.contains(ids, todo.id)) {
-          return _.extend(_.clone(todo), { completed: toggle.completed })
-        }
-        return todo
+        return _.extend(_.clone(todo), { completed: toggle })
       })
     }
 
     this.todoAdded = new Bacon.Bus()
     this.todoModified = new Bacon.Bus()
     this.clearCompleted = new Bacon.Bus()
-    this.toggleCompleted = new Bacon.Bus()
+    this.toggleAll = new Bacon.Bus()
 
     todoChanges = this.todoAdded
                     .map(function(newTodo) { return function(todos) { return todos.concat([newTodo]) }})
                     .merge(this.clearCompleted.map(function() { return function(todos) { return _.where(todos, {completed : false})}}))
                     .merge(this.todoModified.map(function(todo) { return function(todos) { return update(todos, todo) }}))
-                    .merge(this.toggleCompleted.map(function(toggle) { return function(todos) { return toggleCompleted(todos, toggle)}}))
+                    .merge(this.toggleAll.map(function(toggle) { return function(todos) { return toggleCompleted(todos, toggle)}}))
 
     this.allTodos = todoChanges.scan([], function(todos, f) { return f(todos) })
     this.activeTodos = this.allTodos.map(function(todos) { return _.where(todos, { completed: false})})
@@ -129,12 +118,13 @@ $(function() {
 
   function TodoApp() {
     model = new TodoListModel()
-    var filter = FilterView($("#filters"), model)
-    TodoListView($("#todo-list"), model, filter.selectedTodos, filter.changes)
+    var hash = Bacon.UI.hash("#/")
+    FilterView($("#filters"), hash)
+    TodoListView($("#todo-list"), model, hash)
     NewTodoView($("#new-todo"), model)
     ClearCompletedView($("#clear-completed"), model)
     TodoCountView($("#todo-count"), model)
-    ToggleAllView($("#toggle-all"), model, filter.selectedTodos)
+    ToggleAllView($("#toggle-all"), model)
     model.allTodos.map(nonEmpty).assign($("#main,#footer"), "toggle")
   }
 
