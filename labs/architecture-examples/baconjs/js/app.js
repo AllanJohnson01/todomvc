d4135bd098476ff86f84913b64ca86d15aa8c0e3
Juha Paananen <juha.paananen@gmail.com>
2013-01-31 21:33:38 +0200

    refactor into App, Views and Model

--- labs/architecture-examples/baconjs/js/app.js
+++ labs/architecture-examples/baconjs/js/app.js
@@ -1,59 +1,97 @@
 $(function() {
-  var KEYCODE_ENTER = 13
-  todoTemplate = Handlebars.compile($("#todo-template").html())
-  $newTodo = $("#new-todo")
-  $todoList = $("#todo-list")
   function enterKey(element) {
+    var KEYCODE_ENTER = 13
     return element.asEventStream("keyup").filter(function(e) { return e.keyCode == KEYCODE_ENTER })
   }
-  function update(todos, updatedTodo) {
-    return _.map(todos, function(todo) { return todo.id === updatedTodo.id ? updatedTodo : todo })
-  }
-  function renderTodo(newTodo) {
-    $newTodo.val("")
-    var todoElement = $(todoTemplate(newTodo))
+  
+  function TodoView(todo) {
+    var todoTemplate = Handlebars.compile($("#todo-template").html())
+    var todoElement = $(todoTemplate(todo))
     var $label = todoElement.find("label")
     var $editor = todoElement.find(".edit")
-    var title = Bacon.UI.textFieldValue($editor, newTodo.title)
-    var completed = Bacon.UI.checkBoxValue(todoElement.find(".toggle"), newTodo.completed)
+    var title = Bacon.UI.textFieldValue($editor, todo.title)
+    var completed = Bacon.UI.checkBoxValue(todoElement.find(".toggle"), todo.completed)
 
-    $todoList.append(todoElement)
     todoElement.asEventStream("dblclick").merge(enterKey($editor)).onValue(function() { todoElement.toggleClass("editing") })
     title.assign($label, "text")
 
     var todoProperty = Bacon.combineTemplate({
-      id: newTodo.id,
+      id: todo.id,
       title: title,
       completed: completed
     })
-    todoModified.plug(todoProperty.changes())
+
+    return {
+      changes: todoProperty.changes(),
+      element: todoElement
+    }
   }
-  newTodoId = enterKey($newTodo).scan(0, function(id,_) { return id + 1 })
-  todoAdded = Bacon.combineTemplate({ 
-    id: newTodoId,
-    title: Bacon.UI.textFieldValue($newTodo),
-    completed: false
-  }).sampledBy(newTodoId.changes())
-  todoModified = new Bacon.Bus()
-  clearCompleted = $("#clear-completed").asEventStream("click")
 
-  todoChanges = todoAdded
-                  .map(function(newTodo) { return function(todos) { return todos.concat([newTodo]) }})
-                  .merge(clearCompleted.map(function() { return function(todos) { return _.where(todos, {completed : false})}}))
-                  .merge(todoModified.map(function(todo) { return function(todos) { return update(todos, todo) }}))
+  function TodoListView(listElement, model) {
+    model.clearCompleted.map(model.allTodos).onValue(function(todos) {
+      listElement.children().remove()
+      _.each(todos, addTodo)
+    })
 
-  allTodos = todoChanges.scan([], function(todos, f) { return f(todos) })
+    model.todoAdded.onValue(function(todo) {
+      addTodo(todo)
+    })
 
-  activeTodos = allTodos.map(function(todos) { return _.where(todos, { completed: false})})
-  completedTodos = allTodos.map(function(todos) { return _.where(todos, { completed: true})})
+    function addTodo(todo) {
+      var view = TodoView(todo)
+      listElement.append(view.element)
+      model.todoModified.plug(view.changes)
+    }
+  }
 
-  repaint = clearCompleted
+  function NewTodoView(element, model) {
+    var newTodoId = enterKey(element).scan(0, function(id,_) { return id + 1 })
+    var todoAdded = Bacon.combineTemplate({ 
+      id: newTodoId,
+      title: Bacon.UI.textFieldValue(element),
+      completed: false
+    }).sampledBy(newTodoId.changes())
+    todoAdded.onValue(function() { element.val("") })
+    model.todoAdded.plug(todoAdded)
 
-  repaint.map(allTodos).onValue(function(todos) {
-    $todoList.children().remove()
-    _.each(todos, renderTodo)
-  })
-  todoAdded.onValue(renderTodo)
-  activeTodos.map(".length").assign($("#todo-count strong"), "text")
-  completedTodos.map(function(todos) { return todos.length > 0 }).assign($("#clear-completed"), "toggle")
+  }
+
+  function ClearCompletedView(element, model) {
+    model.completedTodos.map(function(todos) { return todos.length > 0 }).assign(element, "toggle")
+    model.clearCompleted.plug(element.asEventStream("click"))
+  }
+
+
+  function TodoCountView(element, model) {
+     model.activeTodos.map(".length").assign(element.find("strong"), "text")
+  }
+
+  function TodoListModel() {
+    function update(todos, updatedTodo) {
+      return _.map(todos, function(todo) { return todo.id === updatedTodo.id ? updatedTodo : todo })
+    }
+
+    this.todoAdded = new Bacon.Bus()
+    this.todoModified = new Bacon.Bus()
+    this.clearCompleted = new Bacon.Bus()
+
+    todoChanges = this.todoAdded
+                    .map(function(newTodo) { return function(todos) { return todos.concat([newTodo]) }})
+                    .merge(this.clearCompleted.map(function() { return function(todos) { return _.where(todos, {completed : false})}}))
+                    .merge(this.todoModified.map(function(todo) { return function(todos) { return update(todos, todo) }}))
+
+    this.allTodos = todoChanges.scan([], function(todos, f) { return f(todos) })
+    this.activeTodos = this.allTodos.map(function(todos) { return _.where(todos, { completed: false})})
+    this.completedTodos = this.allTodos.map(function(todos) { return _.where(todos, { completed: true})})
+  }
+
+  function TodoApp() {
+    var model = new TodoListModel()
+    TodoListView($("#todo-list"), model)
+    NewTodoView($("#new-todo"), model)
+    ClearCompletedView($("#clear-completed"), model)
+    TodoCountView($("#todo-count"), model)
+  }
+
+  TodoApp()
 })
